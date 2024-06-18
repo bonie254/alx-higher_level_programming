b7c1b57070c64305c92743591f2f7383afa3eb12
bonie254 <kdtum0344@gmail.com>
2024-06-18 23:04:20 +0300

    javaWarmup

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-const line1 ="Cis fun";
-const line2="python is great";
-const line3="JavaScript is amazing";
-console.log(line1);
-console.log(line2);
-console.log(line3);
-
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let arr =["C is fun","Python is cool","JavaScript is amazing"];
+
+let x = 0;
+
+while (x<=arr.length-1){
+  console.log(arr[x]);
+  x++;
+}
